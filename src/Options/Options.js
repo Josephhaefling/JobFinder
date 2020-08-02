063b18576ea456b0868c218a24ea5095dbbf7a8e
Joe Haefling <josephhaefling@gmail.com>
2020-08-02 11:14:54 -0600

    Write Options unit tests

--- src/Options/Options.js
+++ src/Options/Options.js
@@ -8,7 +8,7 @@ function Options() {
   return (
     <section data-testid="options-page" className="options-page">
       <h1 className="options-greeting">What would you like to do today?</h1>
-      <img src={stink}  className="user-image"/>
+      <img src={stink}  data-testid="user-image" className="user-image"/>
       <button className="option-btn">Completed Jobs</button>
       <button className="option-btn">Make Schedule</button>
       <button className="option-btn">Favorite Jobs</button>
