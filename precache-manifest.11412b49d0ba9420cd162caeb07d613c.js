5537586d774078d881a6d54ce8243369ee21114e
Joe Haefling <josephhaefling@gmail.com>
2020-08-03 19:46:29 -0600

    Updates

---
+++ precache-manifest.11412b49d0ba9420cd162caeb07d613c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdcc5995f71c86d4ca7c1f8c9d748a47",
+    "url": "/%7BNine2Five%7D/index.html"
+  },
+  {
+    "revision": "6c1518908fd43cc2fdc6",
+    "url": "/%7BNine2Five%7D/static/css/main.f2e5aa74.chunk.css"
+  },
+  {
+    "revision": "647cfed5c56bf0c39427",
+    "url": "/%7BNine2Five%7D/static/js/2.6bdba583.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%7BNine2Five%7D/static/js/2.6bdba583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1518908fd43cc2fdc6",
+    "url": "/%7BNine2Five%7D/static/js/main.d9bd5fd0.chunk.js"
+  },
+  {
+    "revision": "4dd187c688222b75dde0",
+    "url": "/%7BNine2Five%7D/static/js/runtime-main.8356c56f.js"
+  },
+  {
+    "revision": "01527db48f720420a45cd624fcd71d7f",
+    "url": "/%7BNine2Five%7D/static/media/stinkyBaby.01527db4.png"
+  }
+]);
